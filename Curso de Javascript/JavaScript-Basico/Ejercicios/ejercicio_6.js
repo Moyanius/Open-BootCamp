5e816d88a7774867a3abe6ca92e64f2b76951992
Moyanius <moyanojimenezjose@gmail.com>
2022-08-10 19:51:36 +0200

    Open Bootcamp

--- Curso de Javascript/JavaScript-Basico/Ejercicios/ejercicio_6.js
+++ Curso de Javascript/JavaScript-Basico/Ejercicios/ejercicio_6.js
@@ -12,13 +12,28 @@ listacompra.shift("aceite de girasol")
 console.log(listacompra)
 
 // Una lista de tus 3 películas favoritas (objetos con propiedades: titulo, director, fecha)
+const listaPeliculas = [
+    {pelicula: "La vida es bella", director : "Roberto Benigni", año : 1997},
+    {pelicula: "Cadena perpetua", director : "Frank Darabont", año : 1994},
+    {pelicula: "El padrino", director : "Francis Ford Coppola", año : 1972}
+    
+]
 
-// Una nueva lista que contenga las películas posteriores al 1 de enero de 2010 (utilizando filter)
+// Una nueva lista que contenga las películas posteriores al 1 de enero de 1990 (utilizando filter)
+const peliculasPosteriores = listaPeliculas.filter(pel => pel.año > 1990)
+console.log(peliculasPosteriores)
 
 // Una nueva lista que contenga los directores de la lista de películas original (utilizando map)
+const directoresOriginales = listaPeliculas.map(nom => nom.director)
+console.log(directoresOriginales)
 
 // Una nueva lista que contenga los títulos de la lista de películas original (utilizando map)
+const peliculasOriginales = listaPeliculas.map(nom => nom.pelicula)
+console.log(peliculasOriginales)
 
 // Una nueva lista que concatene la lista de directores y la lista de los títulos (utilizando concat)
+console.log(peliculasOriginales.concat(directoresOriginales))
 
 // Una nueva lista que concatene la lista de directores y la lista de los títulos (utilizando el factor de propagación)
+const listaDirectPeli = [...peliculasOriginales, ...directoresOriginales]
+console.log(listaDirectPeli)
