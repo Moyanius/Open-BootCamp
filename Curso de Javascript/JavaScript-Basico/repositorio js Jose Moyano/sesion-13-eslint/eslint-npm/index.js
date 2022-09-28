86aee159d55c4176f3501002c2ed124862f7bd77
moyanius <moyanojimenezjose@gmail.com>
2022-09-28 09:54:06 +0200

    Open Bootcamp

---
+++ Curso de Javascript/JavaScript-Basico/repositorio js Jose Moyano/sesion-13-eslint/eslint-npm/index.js
@@ -0,0 +1,16 @@
+//Este es el archivo que vamos a utilizar
+
+const nombre = "Gorka";
+
+// En esta linea quiero tner comillas simples (quiero que me desactives la regla de las comillas dobles)
+/* eslint-disable*/ //Este seria para quitarlo totalmente de la regla
+/* eslint-disable quotes*/ //Este seria para quitarlo solo de la regla de las comillas
+/* eslint-disable semi*/ //Este	es para quitarlo solo de la regla de los puntos y comas
+
+const persona2 = 'Maria'
+
+/*eslint-enable*/
+
+const persona3 = "Pepe";
+
+console.log(nombre);
---
+++ Curso de Javascript/JavaScript-Basico/repositorio js Jose Moyano/sesion-13-eslint/index.js
@@ -0,0 +1,9 @@
+const nombre = 'Jose';
+
+    var nombre2 = 'Maria';
+
+    var objeto = {
+    nombre: "Jose",
+    apellido: 'Moyano',
+    nombre: 'Maria'
+    }
